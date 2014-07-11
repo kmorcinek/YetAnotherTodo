fc5062440ecd9af90133861ca9e15774228b6525
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2014-07-11 23:59:52 +0200

    Routing using html5 (without hash)

--- src/KMorcinek.YetAnotherTodo/IndexModule.cs
+++ src/KMorcinek.YetAnotherTodo/IndexModule.cs
@@ -7,20 +7,24 @@ namespace KMorcinek.YetAnotherTodo
 {
     public class IndexModule : NancyModule
     {
+        dynamic IndexPage()
+        {
+            var db = DbRepository.GetDb();
+            var firstTopic = db.UseOnceTo().Query<Topic>().OrderBy(t => t.Id).First();
+
+            if (firstTopic == null)
+                throw new HttpException("No topics created yet.");
+
+            return View["YetAnotherTodo"];
+        }
+
         public IndexModule()
         {
             this.RequiresAuthentication();
 
-            Get["/"] = _ =>
-            {
-                var db = DbRepository.GetDb();
-                var firstTopic = db.UseOnceTo().Query<Topic>().OrderBy(t => t.Id).First();
-
-                if (firstTopic == null)
-                    throw new HttpException("No topics created yet.");
-
-                return View["YetAnotherTodo"];
-            };
+            Get["/"] = _ => { return IndexPage(); };
+            Get["/(.*)"] = _ => { return IndexPage(); };
+            Get["/(.*)/(.*)"] = _ => { return IndexPage(); };
         }
     }
 }
--- src/KMorcinek.YetAnotherTodo/app/app.js
+++ src/KMorcinek.YetAnotherTodo/app/app.js
@@ -1,21 +1,23 @@
-﻿var yetAnotherTodoConfig = function($stateProvider, $urlRouterProvider) {
+﻿var yetAnotherTodoConfig = function ($stateProvider, $urlRouterProvider, $locationProvider) {
     
+    $locationProvider.html5Mode(true);
+
     $urlRouterProvider.otherwise('/');
     
     $stateProvider
         .state('admin', {
             url: '/admin',
-            templateUrl: 'app/view/admin.html',
+            templateUrl: '/app/view/admin.html',
             controller: 'AdminCtrl',
         })
         .state('topic', {
             url: '/:topicId/:anyTopicName',
-            templateUrl: 'app/view/topic.html',
+            templateUrl: '/app/view/topic.html',
             controller: 'TopicCtrl',
         })
         .state('home', {
             url: '/',
-            templateUrl: 'app/view/topic.html',
+            templateUrl: '/app/view/topic.html',
             controller: 'TopicCtrl',
         });
 };
