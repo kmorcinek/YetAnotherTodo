85a2dc94bd31e2946accc03b56afd5134e1e74a1
michalbaster <michal.baster@gmail.com>
2014-10-08 22:36:34 +0200

    Project DomainClasses
    Project DomainClasses

---
+++ src/KMorcinek.YetAnotherTodo.DomainClasses/Note.cs
@@ -0,0 +1,11 @@
+﻿namespace KMorcinek.YetAnotherTodo.DomainClasses
+{
+    public class Note
+    {
+        public int NoteId { get; set; }
+        public string Content { get; set; }
+        public int TopicId { get; set; }
+
+        public virtual Topic Topic { get; set; }
+    }
+}
---
+++ src/KMorcinek.YetAnotherTodo.DomainClasses/Topic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace KMorcinek.YetAnotherTodo.DomainClasses
+{
+    public class Topic
+    {
+        private ICollection<Note> _notes;
+
+        public Topic()
+        {
+            _notes = new List<Note>();
+        }
+
+        public int TopicId { get; set; }
+        public string Name { get; set; }
+        public bool IsShown { get; set; }
+
+        public virtual ICollection<Note> Notes
+        {
+            get { return _notes; }
+            set { _notes = value; }
+        }
+    }
+}
