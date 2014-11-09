1cba351d174e880a09297892f6caa40062d3685d
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2014-11-09 22:15:15 +0100

    Exceed time when checking pass, dummy protection from brute force

--- src/KMorcinek.YetAnotherTodo/UserValidator.cs
+++ src/KMorcinek.YetAnotherTodo/UserValidator.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Threading;
 using System.Web;
 using Nancy.Authentication.Basic;
 using Nancy.Security;
@@ -13,6 +15,8 @@ namespace KMorcinek.YetAnotherTodo
                 Path.Combine(
                 HttpContext.Current.Request.PhysicalApplicationPath, 
                 "App_Data/user.txt"));
+
+            Thread.Sleep(TimeSpan.FromSeconds(2));
 
             if (username == lines[0] && password ==  lines[1])
             {
